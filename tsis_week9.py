d42be34b71f1f12a6a79b69f51be8deaa2ca41c1
tokesh <tokesh789@gmail.com>
2021-04-04 22:47:09 +0600

    edit all commits, final version of my tsis week 9

--- tsis_week9.py
+++ tsis_week9.py
@@ -1,9 +1,12 @@
 import pygame,math
 #pylint: disable=no-member
+
+
 pygame.init()
 pygame.font.init()
 all_fonts = pygame.font.get_fonts()
-#to do - sinxcosx text
+
+
 black = (0, 0, 0)
 white = (255, 255, 255)
 blue = (0, 0, 255)
@@ -20,8 +23,8 @@ cos_color = (10, 113, 255)
 
 height = 750
 width = 800
-amp = 250
-freq = 3
+dlina = 250
+chastota = 3
 size = (1000,780)
 run = True
 font = pygame.font.SysFont("arial",30)
@@ -42,22 +45,24 @@ while run:
 
     
     #sin line
-    pointz = []
+    my_xy= []
     for x in range(0,800):
-        y = int((height/2)+amp*math.sin(freq*((float(x)/width)*(2*math.pi))))
-        pointz.append([x+150,y])
-    pygame.draw.lines(screen, sin_color, False, pointz, 5)
+        y = int((height/2)+dlina*math.sin(chastota*((float(x)/width)*(2*math.pi))))
+        my_xy.append([x+150,y])
+    pygame.draw.lines(screen, sin_color, False, my_xy, 5)
 
 
     #cos line
-    pointss = []
+    my_xy_cos = []
     for x in range(0,800,2):
-        y = int((height/2) + amp*math.cos(freq*((float(x)/width)* (2*math.pi))))
-        pointss.append([x+150,y])
-    for i in range(0,len(pointss)-1,3):
-        pygame.draw.line(screen, cos_color, (pointss[i][0],pointss[i][1]), (pointss[i+2][0],pointss[i+2][1]),3)
+        y = int((height/2) + dlina*math.cos(chastota*((float(x)/width)* (2*math.pi))))
+        my_xy_cos.append([x+150,y])
+    for i in range(0,len(my_xy_cos)-1,3):
+        pygame.draw.line(screen, cos_color, (my_xy_cos[i][0],my_xy_cos[i][1]), (my_xy_cos[i+2][0],my_xy_cos[i+2][1]),3)
+
 
     #all lines in graph
+
     #horizontal
     for iq in range(122,689,63):
         pygame.draw.line(screen, black, (115,iq),(990,iq), 2)
@@ -66,6 +71,8 @@ while run:
         if iq ==710: pygame.draw.line(screen, black, (iq,185),(iq,650),2)
         else: pygame.draw.line(screen, black, (iq,100),(iq,650), 2)
     pygame.draw.line(screen, black, (950,100),(950,650),2)
+
+
     #left text
     for iq in range(105,665,63):
         screen.blit(font.render(left_points[help_i], True, black),(35,iq)) # text 0
@@ -76,6 +83,7 @@ while run:
         screen.blit(font_for_bottom_panel.render(bottom_points[help_bot], True, black),(iq,650)) # text 0
         help_bot += 1
     help_bot = 0
+
 
     #left 3 small lines
     for iq in range(122,600,63):
@@ -106,12 +114,19 @@ while run:
         pygame.draw.line(screen, black, (iq+15,100),(iq+15,110), 1)
         pygame.draw.line(screen, black, (iq+30,100),(iq+30,107), 1)
         help_range += 1
+
+
+    #drobi   
     for iq in range(210, 980,140):
         screen.blit(font_for_bottom_panel.render('--', True, black),(iq,659)) # drob
     for iq in range(210, 980,140):
         screen.blit(font_for_bottom_panel.render('2', True, black),(iq,672)) # 2
-    screen.blit(font_for_bottom_panel.render('X', True, black),(565,690)) # X in the bottom
 
+    #x in the bottom
+    screen.blit(font_for_bottom_panel.render('X', True, black),(565,690))
+
+
+    #text in graph, with lines
     screen.blit(font_for_text_in_square.render('sin x', True, black),(660,120)) # sin x
     screen.blit(font_for_text_in_square.render('cos x', True, black),(660,145)) # cos x
     pygame.draw.line(screen, sin_color, (710,135),(750,135),3)
@@ -119,5 +134,8 @@ while run:
     pygame.draw.line(screen, cos_color, (725,160),(735,160),3)
     pygame.draw.line(screen, cos_color, (740,160),(750,160),3)
     pygame.draw.rect(screen,black, (115,100,875,550), 4)
+
+
+
     pygame.display.flip()
 pygame.quit()
