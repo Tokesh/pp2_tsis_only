e670d151a1598963420c1375770519b342ed3995
tokesh <tokesh789@gmail.com>
2021-04-23 00:35:52 +0600

    full completed edition of my last tsis, Asman the best <3

--- tsis_week12/main_tsis.py
+++ tsis_week12/main_tsis.py
@@ -1,12 +1,13 @@
 import psycopg2
 from config import config
 """ 
-v Connecting to the PostgreSQL database server
-v Creating new PostgreSQL tables in Python
-v Inserting data into the PostgreSQL table in Python
-v Updating data in the PostgreSQL table in Python
-v Querying data from the PostgreSQL tables
-v Deleting data from PostgreSQL tables in Python 
+vv Connecting to the PostgreSQL database server
+vv Creating new PostgreSQL tables in Python
+vv Inserting data into the PostgreSQL table in Python
+vv Updating data in the PostgreSQL table in Python
+vv Querying data from the PostgreSQL tables
+vv Deleting data from PostgreSQL tables in Python 
+vv Not to restart everytime
 """
 commands = list()
 
@@ -37,17 +38,55 @@ def create_data_table():
         if k[-1] !=',': break
         
     create_table += ");"
-    print(create_table)
     commands.append(create_table)
     actions()
+    zxc = input("""Please choose variant here!
+    1 <--- Creating new table
+    2 <--- Inserting data into table
+    3 <--- Updating data in the table
+    4 <--- Querying data from the table
+    5 <--- Deleting data from table
+    6 <--- Exit from programm
+    """)
+    if zxc =='1': create_data_table()
+    elif zxc == '2': insert_data_table()
+    elif zxc == '3': update_data_table()
+    elif zxc == '4': querying_data_table()
+    elif zxc == '5': drop_data_table()
+    elif zxc == '6': exit(0)
+
 
 
 def drop_data_table():
     name_of_table = input("Input the name of the table: ")
-    selected_data = input("Write the name which you want to delete: ")
-    drop_data = """delete from {} where first_name='{}';""".format(name_of_table, selected_data)
+    name_of_column = input("Write the name of column where you want to delete data: ")
+    data_of_column = input("Input the data, which you want to delete: ")
+    drop_data = """delete from {} where {}='{}'""".format(name_of_table, name_of_column, data_of_column)
+    while True:
+        add_condition = input("""Did you want to add condition?
+        1 - YES
+        2 - NO
+        """)
+        if add_condition == '2': break
+        else: k = input("Add some conditions like: and role_name = '666': ")
+        drop_data += k
+    drop_data += ';'
     commands.append(drop_data)
     actions()
+    zxc = input("""Please choose variant here!
+    1 <--- Creating new table
+    2 <--- Inserting data into table
+    3 <--- Updating data in the table
+    4 <--- Querying data from the table
+    5 <--- Deleting data from table
+    6 <--- Exit from programm
+    """)
+    if zxc =='1': create_data_table()
+    elif zxc == '2': insert_data_table()
+    elif zxc == '3': update_data_table()
+    elif zxc == '4': querying_data_table()
+    elif zxc == '5': drop_data_table()
+    elif zxc == '6': exit(0)
 
 
 def querying_data_table():
@@ -70,15 +109,54 @@ def querying_data_table():
         print(str(e))
     if conn is not None:
         conn.close()
+    zxc = input("""Please choose variant here!
+    1 <--- Creating new table
+    2 <--- Inserting data into table
+    3 <--- Updating data in the table
+    4 <--- Querying data from the table
+    5 <--- Deleting data from table
+    6 <--- Exit from programm
+    """)
+    if zxc =='1': create_data_table()
+    elif zxc == '2': insert_data_table()
+    elif zxc == '3': update_data_table()
+    elif zxc == '4': querying_data_table()
+    elif zxc == '5': drop_data_table()
+    elif zxc == '6': exit(0)
 
 def update_data_table():
+    print("To update data, we using this format: i want to change this DATA = new_data where ADDRESS==? ")
     name_of_table = input("Input the name of the table: ")
-    last_name_of_pupil = input("Input the old name of the Student: ")
-    new_name_of_pupil = input("Input the new name of the Student: ")
-    update_data = """update {} set first_name = '{}' where first_name = '{}';""".format(name_of_table, new_name_of_pupil, last_name_of_pupil)
+    update_name_data = input("Insert name of column where you want to update data: ")
+    update_info_data = input("Insert the data what you want to update: ")
+    address = input("Input the ADDRESS: ")
+    address_data = input("Input the information of this ADDRESS: ")
+    update_data = """update {} set {} = '{}' where {} = '{}'""".format(name_of_table, update_name_data, update_info_data, address, address_data)
+    while True:
+        add_condition = input("""Did you want to add condition?
+        1 - YES
+        2 - NO
+        """)
+        if add_condition == '2': break
+        else: k = input("Add some conditions like: and role_name = '666': ")
+        update_data += k
+    update_data += ';'
     commands.append(update_data)
     actions()
-
+    zxc = input("""Please choose variant here!
+    1 <--- Creating new table
+    2 <--- Inserting data into table
+    3 <--- Updating data in the table
+    4 <--- Querying data from the table
+    5 <--- Deleting data from table
+    6 <--- Exit from programm
+    """)
+    if zxc =='1': create_data_table()
+    elif zxc == '2': insert_data_table()
+    elif zxc == '3': update_data_table()
+    elif zxc == '4': querying_data_table()
+    elif zxc == '5': drop_data_table()
+    elif zxc == '6': exit(0)
 
 def insert_data_table():
     name_of_table = input("Input the name of the table: ")
@@ -87,6 +165,20 @@ def insert_data_table():
     insert_data = """insert into {} values({})""".format(name_of_table, name_of_input)
     commands.append(insert_data)
     actions()
+    zxc = input("""Please choose variant here!
+    1 <--- Creating new table
+    2 <--- Inserting data into table
+    3 <--- Updating data in the table
+    4 <--- Querying data from the table
+    5 <--- Deleting data from table
+    6 <--- Exit from programm
+    """)
+    if zxc =='1': create_data_table()
+    elif zxc == '2': insert_data_table()
+    elif zxc == '3': update_data_table()
+    elif zxc == '4': querying_data_table()
+    elif zxc == '5': drop_data_table()
+    elif zxc == '6': exit(0)
 
 
 def actions():    
